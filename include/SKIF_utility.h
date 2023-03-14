8e6c808ffa6fd1d728357330baf96e75244905a3
aemony <aemony@aemony.se>
2023-03-14 06:57:46 +0100

    Added a comment about GetErrorAsWStr()

--- include/SKIF_utility.h
+++ include/SKIF_utility.h
@@ -11,7 +11,7 @@
 
 std::string  SKIF_Util_ToLower               (std::string_view  input);
 std::wstring SKIF_Util_TowLower              (std::wstring_view input);
-std::wstring SKIF_Util_GetErrorAsWStr        (DWORD error = GetLastError ( ));
+std::wstring SKIF_Util_GetErrorAsWStr        (DWORD error = GetLastError ( )); // Handles System Error Codes, https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes#system-error-codes
 DWORD        SKIF_Util_timeGetTime           (void);
 std::wstring SKIF_Util_timeGetTimeAsWStr     (const std::wstring& format = L"H:M:s.m");
 int          SKIF_Util_CompareVersionStrings (std::wstring string1, std::wstring string2);
