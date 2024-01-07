bfdaa69475ebb36920a7bdb126c0743e63b147c0
aemony <aemony@aemony.se>
2024-01-07 20:45:53 +0100

    Removed unnecessary definitions

--- src/utility/utility.cpp
+++ src/utility/utility.cpp
@@ -625,14 +625,12 @@ SKIF_Util_OpenURI (
 
   //UINT flags =   SEE_MASK_FLAG_NO_UI | SEE_MASK_NOZONECHECKS
 
-#if 1
   PLOG_VERBOSE                           << "Performing a ShellExecute call...";
   PLOG_VERBOSE_IF(! path.empty())        << "File      : " << path;
   PLOG_VERBOSE_IF(verb       != nullptr) << "Verb      : " << std::wstring(verb);
   PLOG_VERBOSE_IF(parameters != nullptr) << "Parameters: " << std::wstring(parameters);
   PLOG_VERBOSE_IF(directory  != nullptr) << "Directory : " << std::wstring(directory);
 //PLOG_VERBOSE                           << "Flags     : " << flags;
-#endif
 
   if (_registry._LoadedSteamOverlay)
     SetEnvironmentVariable (L"SteamNoOverlayUIDrawing", NULL);
