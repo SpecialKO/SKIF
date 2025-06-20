ed2476fc16f9e40ec3ebd33238586418f5e870fa
Aemony <aemony@aemony.se>
2025-06-20 10:56:10 +0200

    Fix ExplorePath not opening some paths
    A full path to the directory, including a final "\" at the end, is
    require for Explorer to actually open some paths in some situations,
    as otherwise it might try to open another path (e.g. "\directory.lnk").
    
    Also remove unnecessary flags that only suppressed visible errors.

--- include/utility/utility.h
+++ include/utility/utility.h
@@ -132,7 +132,7 @@ struct SKIF_Util_CreateProcess_s {
   std::atomic<int>    iReturnCode   = -1; // Could the separate process be spawned through CreateProcess ? (0 == NO_ERROR; 1+ == ERROR_xxx)
 };
 
-HINSTANCE       SKIF_Util_ExplorePath                 (const std::wstring_view& path);
+HINSTANCE       SKIF_Util_ExplorePath                 (std::wstring path);
 HINSTANCE       SKIF_Util_OpenURI                     (const std::wstring_view& path, int nShow = SW_SHOWNORMAL, LPCWSTR verb      = L"OPEN", LPCWSTR parameters = NULL, LPCWSTR directory = NULL, UINT flags = SEE_MASK_FLAG_NO_UI | SEE_MASK_NOZONECHECKS);
 bool            SKIF_Util_ShellExecuteUnelevated      (const std::wstring_view& path, int nShow = SW_SHOWNORMAL, LPCWSTR verb      = L"OPEN", LPCWSTR parameters = NULL, LPCWSTR directory = NULL);
 bool            SKIF_Util_CreateProcess               (const std::wstring_view& path, const std::wstring_view& parameters, const std::wstring_view& directory, std::map<std::wstring, std::wstring>* env = nullptr, SKIF_Util_CreateProcess_s* proc = nullptr);
