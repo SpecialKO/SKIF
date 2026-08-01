c97a743a8bf18d99d6026f9870a1d27eba0d5aee
Aemony <code.aemony@proton.me>
2026-08-01 13:09:18 +0200

    Make custom games support working directory

--- include/stores/SKIF/custom_library.h
+++ include/stores/SKIF/custom_library.h
@@ -3,6 +3,6 @@
 #include <stores/Steam/app_record.h>
 
 bool                  SKIF_RemoveCustomAppID (uint32_t appid);
-int                   SKIF_AddCustomAppID    (std::wstring  name, std::wstring      path, std::wstring      args);
-bool                  SKIF_ModifyCustomAppID (app_record_s* pApp, std::wstring_view path, std::wstring_view args);
+int                   SKIF_AddCustomAppID    (std::wstring  name, std::wstring      exePath, std::wstring      args, std::wstring workingPath);
+bool                  SKIF_ModifyCustomAppID (app_record_s* pApp, std::wstring_view exePath, std::wstring_view args, std::wstring workingPath);
 void                  SKIF_GetCustomAppIDs   (std::vector <std::pair < std::string, app_record_s > > *apps);
